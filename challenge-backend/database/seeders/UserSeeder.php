9ae99199c4ef43e94c8f8d558470265bc5141abb
rashu-raja <rashurajajabegu@gmail.com>
2026-04-20 19:03:33 +0545

    "forget this file earlier"

---
+++ challenge-backend/database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::firstOrCreate(
+            ['email' => env('ADMIN_EMAIL')],
+            [
+                'name' => 'admin',
+                'password' => Hash::make(env('ADMIN_PASSWORD')),
+                'role' => 'admin'
+            ]
+        );
+
+        User::factory(200)->create();
+    }
+}
